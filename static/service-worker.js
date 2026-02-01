0f5e8f7b8f8a2c35895c5498f8338d1f665f1b5f
Antonio Belmonte <tonybelmonte331@gmail.com>
2026-02-01 17:34:38 -0500

    Initial PWA commit

---
+++ static/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "handicap-v1";
+const urlsToCache = [
+  "/",
+  "/static/style.css"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
